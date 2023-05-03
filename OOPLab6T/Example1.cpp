e7f6da0074a9221df3180152b583e3fe4efc2dd4
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2023-05-03 22:59:05 +0300

    task 0.3

--- OOPLab6T/Example1.cpp
+++ OOPLab6T/Example1.cpp
@@ -14,6 +14,13 @@ namespace SpaceExample1 {
     public:
         Base() : dat(1) {}
         Base(int d) : dat(d) {}
+        Base(int a1, int a2, int a3, double a4, int a5) {
+            a[0] = a1;
+            a[1] = a2;
+            a[2] = a3;
+            a[3] = a4;
+            a[4] = a5;
+        }
     };
   
     class D1 : protected Base
@@ -24,6 +31,8 @@ namespace SpaceExample1 {
         D1() : d1(1) {}
         D1(int d) : d1(d) {}
         D1(int d, int dt) : Base(dt), d1(d) {}
+        D1(int a, int b, int c, double d, int e) : Base(a, b, c, d, e), d1(d) {}
+
     };
 
     class D2 : protected Base
@@ -36,24 +45,24 @@ namespace SpaceExample1 {
         D2(int d, double dt) : Base(d), d2(dt) {}
     };
 
-    class D12 : protected D1, protected D2
+    class D3 : protected D1, protected D2
     {
     protected:
         double dt;
     public:
-        D12() : dt(1) {}
-        D12(int d) : dt(d) {}
-        D12(int a, int b, int c, double d, int e) : D1(a, b), D2(c, d), dt(e) {}
+        D3() : dt(1) {}
+        D3(int d) : dt(d) {}
+        D3(int a, int b, int c, double d, int e) : D1(a, b), D2(c, d), dt(e) {}
     };
 
-    class R : protected D12, protected Base
+    class D4 : protected D1
     {
     protected:
         double dt;
     public:
-        R() : dt(1) {}
-        R(int d) : dt(d) {}
-        R(int a, int b, int c, double d, int e) : D12(a, b, c, d, e), Base::Base(a), dt(e + 1.) {}
+        D4() : dt(1) {}
+        D4(int d) : dt(d) {}
+        D4(int a, int b, int c, double d, int e) : D1(a, b, c, d, e) {}
         void showDat()
         {
             std::cout << "dat =? Error C2385 ambiguous access level dat " << std::endl;
@@ -64,9 +73,28 @@ namespace SpaceExample1 {
         }
     };
     
-    //
-    //  virtual
-    //
+
+    class D5 : protected D1, protected D3
+    {
+    protected:
+        int d5;
+    public:
+        D5() : d5(1) {}
+        D5(int d) : d5(d) {}
+        D5(int d, int dt) : D1(d), D3(d), d5(d) {}
+    };
+
+    class D6 : protected D2, protected D4
+    {
+    protected:
+        int d6;
+    public:
+        D6() : d6(1) {}
+        D6(int d) : d6(d) {}
+        D6(int d, int dt) : D2(d), D4(d), d6(d) {}
+    };
+    /*****virtual*****/
+
     class D1V : virtual protected Base
     {
     protected:
@@ -75,6 +103,7 @@ namespace SpaceExample1 {
         D1V() : d1(1) {}
         D1V(int d) : d1(d) {}
         D1V(int d, int dt) : Base(dt), d1(d) {}
+        D1V(int a, int b, int c, double d, int e) : Base(a, b, c, d, e), d1(d) {}
     };
 
     class D2V : virtual protected Base
@@ -87,38 +116,90 @@ namespace SpaceExample1 {
         D2V(int d, double dt) : Base(d), d2(dt) {}
     };
 
-    class D12VV : virtual protected D1V, virtual public D2V
+    class D3V :virtual protected D1V, virtual protected D2V
     {
     protected:
         double dt;
     public:
-        D12VV() : dt(1) {}
-        D12VV(int d) : dt(d) {}
-        D12VV(int a, int b, int c, double d, int e) : D1V(a, b), D2V(c, d), dt(e) {}
+        D3V() : dt(1) {}
+        D3V(int d) : dt(d) {}
+        D3V(int a, int b, int c, double d, int e) : D1V(a, b), D2V(c, d), dt(e) {}
     };
 
-    class RV3 : virtual protected D12VV, virtual public Base
+    class D4V :virtual protected D1V
     {
     protected:
         double dt;
     public:
-        RV3() : dt(1) {}
-        RV3(int d) : dt(d) {}
-        RV3(int a, int b, int c, double d, int e) : D12VV(a, b, c, d, e), Base::Base(a + 1),
-            dt(e + 1.0) {}
-        void showDat()
-        {
-            std::cout << "  dat =  " << dat << std::endl;
-            std::cout << "B12VV::D1V::Base::dat =  " << D12VV::D1V::Base::dat << std::endl;
-            std::cout << "B12VV::D1V::Base::dat =  " << Base::dat << std::endl;
-            std::cout << "B12VV::D1V::Base::dat =  " << D12VV::D2V::Base::dat << std::endl;
-        }
+        D4V() : dt(1) {}
+        D4V(int d) : dt(d) {}
+        D4V(int a, int b, int c, double d, int e) : D1V(a, b, c, d, e) {}
     };
+
+
+    class D5V : virtual protected D1V, virtual protected D3V
+    {
+    protected:
+        int d5;
+    public:
+        D5V() : d5(1) {}
+        D5V(int d) : d5(d) {}
+        D5V(int d, int dt) : D1V(d), D3V(d), d5(d) {}
+    };
+
+    class D6V : virtual protected D2V, virtual protected D4V
+    {
+    protected:
+        int d6;
+    public:
+        D6V() : d6(1) {}
+        D6V(int d) : d6(d) {}
+        D6V(int d, int dt) : D2V(d), D4V(d), d6(d) {}
+    };
+
+    //class D2V : virtual protected Base
+    //{
+    //protected:
+    //    double d2;
+    //public:
+    //    D2V() : d2(1) {}
+    //    D2V(int d) : d2(d) {}
+    //    D2V(int d, double dt) : Base(d), d2(dt) {}
+    //};
+
+    //class D12VV : virtual protected D1V, virtual public D2V
+    //{
+    //protected:
+    //    double dt;
+    //public:
+    //    D12VV() : dt(1) {}
+    //    D12VV(int d) : dt(d) {}
+    //    D12VV(int a, int b, int c, double d, int e) : D1V(a, b), D2V(c, d), dt(e) {}
+    //};
+
+    //class RV3 : virtual protected D12VV, virtual public Base
+    //{
+    //protected:
+    //    double dt;
+    //public:
+    //    RV3() : dt(1) {}
+    //    RV3(int d) : dt(d) {}
+    //    RV3(int a, int b, int c, double d, int e) : D12VV(a, b, c, d, e), Base::Base(a + 1),
+    //        dt(e + 1.0) {}
+    //    void showDat()
+    //    {
+    //        std::cout << "  dat =  " << dat << std::endl;
+    //        std::cout << "B12VV::D1V::Base::dat =  " << D12VV::D1V::Base::dat << std::endl;
+    //        std::cout << "B12VV::D1V::Base::dat =  " << Base::dat << std::endl;
+    //        std::cout << "B12VV::D1V::Base::dat =  " << D12VV::D2V::Base::dat << std::endl;
+    //    }
+    //};
 
     int mainExample1()
     {
         std::cout << " Example1  \n";
-        R a, b(1, 2, 3, 4.5, 5);
+        D5 a, b(1, 2, 3, 4.5, 5);
+        D6 a, b(1, 2, 3, 4.5, 5);
         RV3 av, bv(1, 2, 3, 4.5, 5);
 
         std::cout << "Test !\n";
