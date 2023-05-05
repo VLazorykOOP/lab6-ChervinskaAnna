3fb36542dd86834811d6586e0c61b060b1073cb1
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2023-05-05 21:40:03 +0300

    Final

--- OOPLab6T/Example3.cpp
+++ OOPLab6T/Example3.cpp
@@ -25,10 +25,6 @@ namespace SpaceExample3 {
         cout << " Number of crew: " << n.NumCrew << endl << " Type of transport " << n.TypeTransport << endl;
         return os;
     }
-    istream& operator>>(istream& iso, Transport& n) {
-        cin >> n.NumCrew >> n.TypeTransport;
-        return iso;
-    }
 
 
     class PassengerTransport : public Transport {
@@ -45,11 +41,7 @@ namespace SpaceExample3 {
         cout << " Number of crew: " << n.NumCrew << endl << " Type of transport " << n.TypeTransport << endl << " Number of Passengers " << n.NumPassengers << endl;
         return os;
     }
-    istream& operator>>(istream& iso, PassengerTransport& n) {
-        cin >> n.NumCrew >> n.TypeTransport >> n.NumPassengers;
-        return iso;
-    }
-
+ 
     class Ship : public Transport { 
     public:
         int Lifeboat;
@@ -67,33 +59,26 @@ namespace SpaceExample3 {
         cout << " Number of Lifeboat " << n.Lifeboat << endl << " Number of Lifebuoy " << n.Lifebuoy << endl;
         return os;
     }
-    istream& operator>>(istream& iso, Ship& n) {
-        cin >> n.NumCrew >> n.TypeTransport >> n.Lifeboat >> n.Lifebuoy;
-        return iso;
-    }
 
     class PassengerShip : public PassengerTransport, public Ship {
     public:
         int CabinNum;
         PassengerShip() {
         }
-        PassengerShip (int lifeboat, int lifebuoy, int numPassengers, int cabinNum) {
-            this->Lifeboat = lifeboat;
-            this->Lifebuoy = lifebuoy;
-            this->NumPassengers = numPassengers;
+        PassengerShip (int numCrew, string typeTransport, int lifeboat, int lifebuoy, int numPassengers, int cabinNum) 
+            : PassengerTransport(numCrew, typeTransport, numPassengers), Ship(numCrew, typeTransport, lifeboat, lifebuoy)
+                {
             this->CabinNum = cabinNum;
         }
     };
 
     ostream& operator<<(ostream& os, PassengerShip& n) {
-        cout << " Number of Lifeboat " << n.Lifeboat << endl << " Number of Lifeboat " << n.Lifeboat << endl;
+        cout << " Number of crew: " << ( (PassengerTransport) n).NumCrew << endl << " Type of transport " << ((PassengerTransport) n).TypeTransport << endl;
+        cout << " Number of Lifeboat " << n.Lifeboat << endl << " Number of Lifebuoy " << n.Lifebuoy << endl;
         cout << " Number of Passengers " << n.NumPassengers << endl << " Cabin numbers" << n.CabinNum << endl;
         return os;
     }
-    istream& operator>>(istream& iso, PassengerShip& n) {
-        cin >> n.Lifeboat >> n.Lifebuoy >> n.NumPassengers >> n.CabinNum;
-        return iso;
-    }
+   
 
     int mainExample3()
     {
@@ -103,7 +88,7 @@ namespace SpaceExample3 {
         cout << "OBJ2: " << obj2 << endl;
         Ship obj3 = Ship(356, "Ship", 49, 289);
         cout << "OBJ3: " << obj3 << endl;
-        PassengerShip obj4 = PassengerShip(187, 365, 678, 245);
+        PassengerShip obj4 = PassengerShip(265, "Passenger Ship", 187, 365, 678, 245);
         cout << "OBJ4: " << obj4 << endl;
       
         return 0;
